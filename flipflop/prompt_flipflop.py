4cf06dc431a1674fb778af2bacd0b33409784657
Mayank Jobanputra <mayankjobanputra@gmail.com>
2024-11-22 11:32:24 +0100

    code refactoring for future purposes

--- flipflop/prompt_flipflop.py
+++ flipflop/prompt_flipflop.py
@@ -46,26 +46,7 @@ def parse_response(response_text):
     return answer
 
 
-if __name__ == "__main__":
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--ip_path", type=str, required=True, help="Dir Path to the dataset")
-    ap.add_argument("--engine", type=str, required=True, help="Engine to use for inference")
-    ap.add_argument("--save_path", type=str, nargs='?', default="results/flipflop", help="Dir Path to save results in jsonlines")
-    args = ap.parse_args()
-
-    client, model = None, None
-    if "openai" in args.engine:
-        client = openai.Client(
-            base_url="http://127.0.0.1:8080/v1", api_key="sk_noreq")
-    else:
-        raise NotImplementedError("No other engines supported yet")
-
-    with open(args.ip_path) as reader:
-        data = reader.readlines()
-
-    registry = DirectoryPromptRegistry(Path("prompts/flipflop_zero-shot_basic"), force_reindex=True)
-    task_prompt = registry.get(name="task")
-    system_prompt = registry.get(name="sys")
+def openai_single_query(data, client, task_prompt, system_prompt):
 
     responses = []
     for d in data:
@@ -86,5 +67,32 @@ if __name__ == "__main__":
             "full_answer": res_text
         }
         responses.append(response)
+    return responses
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--ip_path", type=str, required=True, help="Dir Path to the dataset")
+    ap.add_argument("--engine", type=str, required=True, help="Engine to use for inference")
+    ap.add_argument("--save_path", type=str, nargs='?', default="results/flipflop", help="Dir Path to save results in jsonlines")
+    args = ap.parse_args()
+
+    with open(args.ip_path) as reader:
+        data = reader.readlines()
+
+    registry = DirectoryPromptRegistry(Path("prompts/flipflop_zero-shot_basic"), force_reindex=True)
+    task_prompt = registry.get(name="task")
+    system_prompt = registry.get(name="sys")
+
+    if not Path(args.save_path).exists():
+        Path(args.save_path).mkdir(parents=True, exist_ok=True)
+
+    client, model = None, None
+    if "openai" in args.engine:
+        client = openai.Client(
+            base_url="http://127.0.0.1:8080/v1", api_key="sk_noreq")
+        responses = openai_single_query(data, client, task_prompt, system_prompt)
+    else:
+        raise NotImplementedError("No other engines supported yet")
 
     save_to_json(args.save_path, responses)
