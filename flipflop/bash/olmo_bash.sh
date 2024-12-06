a5d00e942e3ebf67f4f65c75fd1528eadf6b1dda
Yana Veitsman <yanav@tony-2.coli.uni-saarland.de>
2024-12-06 12:28:46 +0100

    olmo startup script yana

---
+++ flipflop/bash/olmo_bash.sh
@@ -0,0 +1,19 @@
+# Conda/venv commands
+source /scratch/yanav/anaconda3/bin/activate
+conda activate len-gen
+
+# Replace LD_LIBRARY_PATH with the path to the nvjitlink library in the conda environment (not sure if this is needed/not)
+export LD_LIBRARY_PATH=/scratch/yanav/anaconda3/lib/python3.12/site-packages/nvidia/nvjitlink/lib/:$LD_LIBRARY_PATH
+
+# HF model name, appropriate tensor parallel size, ideally check all the parameters
+CUDA_VISIBLE_DEVICES=0,1 vllm serve /scratch/common_models/OLMo-7B-0724-Instruct-hf --tensor-parallel-size 2 --gpu-memory-utilization 0.9 --disable-log-stats --seed 5 --api-key "sk_noreq" --host 0.0.0.0 --port 8080 &
+
+# We want to shut down the VLLM server after the experiment is done, so we need its PID
+VLLMPID=$!
+
+# Run the experiment, possible to run multiple experiments in sequence
+python flipflop/prompt_flipflop.py --ip_path datasets/flipflop/examples.txt --save_path results/flipflop/OLMo_7B/ --engine openai
+
+# Shut down the VLLM server
+kill $VLLMPID
+
