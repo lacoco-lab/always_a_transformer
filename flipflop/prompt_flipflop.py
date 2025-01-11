4e7d6cd3e67ef3a534a2c72771d5b2e3196ff24f
Mayank Jobanputra <mayankjobanputra@gmail.com>
2025-01-11 05:05:14 +0100

    all the new prompts and code changes

--- flipflop/prompt_flipflop.py
+++ flipflop/prompt_flipflop.py
@@ -6,16 +6,19 @@ import openai
 
 from banks.registries import DirectoryPromptRegistry
 
+from utils.inference_constants import get_inference_params
 from utils.vllm_openai_server import batch_chat, wait_for_engine_to_start
-from utils.utils import get_last_write_index, parse_flipflop_response, save_to_jsonl
+from utils.utils import (get_last_write_index, parse_flipflop_response, save_to_jsonl, 
+                         get_flipflop_task_and_type_from_prompt, get_stop_token)
 
 
-def merge_data_with_responses(data, responses):
+def merge_data_with_responses(data, responses, task, task_type):
     output = []
+    stop_token = get_stop_token(task, task_type)
     for r_idx, resp in enumerate(responses):
         d = data[r_idx]
-        res_text = resp.choices[0].message.content
-        answer = parse_flipflop_response(res_text)
+        res_text = resp.choices[0].message.content + stop_token if stop_token != "NA" else resp.choices[0].message.content
+        answer = parse_flipflop_response(res_text, task, task_type)
         last_write_index = get_last_write_index(d.strip())
         # print(f"Question: {d.strip()[:-1]} **** \nAnswer: {answer}\n****\n")
         response = {
@@ -35,6 +38,7 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("--ip_path", type=str, required=True, help="Dir Path to the dataset")
     ap.add_argument("--engine", type=str, required=True, help="Engine to use for inference")
+    ap.add_argument("--prompt_path", type=str, required=True, help="Path to the prompt registry directory")
     ap.add_argument("--save_path", type=str, nargs='?', default="results/flipflop", help="Dir Path to save results in jsonlines")
     args = ap.parse_args()
 
@@ -44,9 +48,12 @@ if __name__ == "__main__":
         else:
             raise ValueError("Invalid input file type")
 
-    registry = DirectoryPromptRegistry(Path("prompts/flipflop_zero-shot_chat_worded"), force_reindex=True)
+    registry = DirectoryPromptRegistry(Path(args.prompt_path), force_reindex=True)
     task_prompt = registry.get(name="task")
     system_prompt = registry.get(name="sys")
+    
+    task, task_type = get_flipflop_task_and_type_from_prompt(args.prompt_path)
+    inference_params = get_inference_params(task, task_type)
 
     if not Path(args.save_path).exists():
         Path(args.save_path).mkdir(parents=True, exist_ok=True)
@@ -58,8 +65,8 @@ if __name__ == "__main__":
         wait_for_engine_to_start(base_url)
         client = openai.AsyncClient(
             base_url=base_url, api_key="sk_noreq", max_retries=10)
-        results = batch_chat(data, client, task_prompt, system_prompt)
-        results = merge_data_with_responses(data, results)
+        results = batch_chat(data, client, task_prompt, system_prompt, inference_params=inference_params)
+        results = merge_data_with_responses(data, results, task, task_type)
     else:
         raise NotImplementedError("No other engines supported yet")
 
