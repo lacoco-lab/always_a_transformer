5263d9f62071669e1593981765917b4ebec591e9
Yana Veitsman <yana.veitsman@yandex.kz>
2025-02-13 14:07:00 +0100

    add prompts

--- api_playground/play_flipflop.py
+++ api_playground/play_flipflop.py
@@ -6,15 +6,15 @@ from prompt_utils import render_prompt
 
 client = Together()
 system_path = "system.jinja"
-task_path = "templates/flipflop-one-w-record.jinja"
-output_file = "results/flipflop-one-w-record/flipflop_500_pw0.1.jsonl"
+task_path = "templates/flipflop-traverse-left.jinja"
+output_file = "results/flipflop-traverse-left/flipflop_500_pw0.1.jsonl"
 flipflop_data = "../results/flipflop/llama3.3_70B-instruct/sparse/s1/flipflop_500_pw0_results.jsonl"
 
 queries = []
 with open(flipflop_data, 'r', encoding='utf-8') as file:
     for line in file:
         data = json.loads(line)
-        system_prompt, task_prompt = render_prompt(system_path, task_path, data['flipflop'])
+        system_prompt, task_prompt = render_prompt(system_path, task_path, data['flipflop'][:-1])
         queries.append(
             {
                 'task_prompt': task_prompt,
