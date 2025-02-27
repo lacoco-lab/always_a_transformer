029a69c9d6822bda9ceef57de41dc28024729246
Mayank Jobanputra <mayankjobanputra@gmail.com>
2025-02-27 17:38:11 +0100

    fixed flipflop mamba

--- inductionhead/bash/500_mamba.sh
+++ inductionhead/bash/500_mamba.sh
@@ -19,21 +19,21 @@ mkdir -p ~/.config/vllm && touch ~/.config/vllm/do_not_track
 
 # For falcon3-mamba-7B-instruct
 
-vllm serve tiiuae/Falcon3-Mamba-7B-Instruct --tensor-parallel-size 4 --gpu-memory-utilization 0.95 --disable-log-stats --seed 5 --api-key "sk_noreq" --host 0.0.0.0 --port $PORT --max-model-len 64000 --max-seq-len-to-capture 128000 --max-num-batched-tokens 128000 &
+vllm serve tiiuae/Falcon3-Mamba-7B-Instruct --tensor-parallel-size 1 --gpu-memory-utilization 0.95 --disable-log-stats --seed 5 --api-key "sk_noreq" --host 0.0.0.0 --port $PORT --max-model-len 64000 --max-seq-len-to-capture 64000 --max-num-batched-tokens 128000 &
 #vllm serve tiiuae/Falcon3-Mamba-7B-Instruct --tensor-parallel-size 2 --gpu-memory-utilization 0.85 --disable-log-stats --seed 5 --api-key "sk_noreq" --host 0.0.0.0 --port $PORT --max-model-len 32000 --max-seq-len-to-capture 32000 --max-num-batched-tokens 48000 &
 
 VLLMPID=$!
 
-python first_ones/prompt_first_ones.py --ip_path datasets/500/first_or_last/data.jsonl --prompt_path prompts/first_ones/zero-shot_chat_first_v0 --cot "nocot" --save_path results/first_ones/falcon3-mamba-7B-instruct --port $PORT
+#python first_ones/prompt_first_ones.py --ip_path datasets/500/first_or_last/data.jsonl --prompt_path prompts/first_ones/zero-shot_chat_first_v0 --cot "nocot" --save_path results/first_ones/falcon3-mamba-7B-instruct --port $PORT
 #python first_ones/prompt_first_ones.py --ip_path datasets/500/first_or_last/data.jsonl --prompt_path prompts/first_ones/zero-shot_chat_first_v0 --cot "cot" --save_path results/first_ones/falcon3-mamba-7B-instruct --port $PORT
 
-python last_ones/prompt_last_ones.py --ip_path datasets/500/first_or_last/data.jsonl --prompt_path prompts/last_ones/zero-shot_chat_last_v0 --cot "nocot" --save_path results/last_ones/falcon3-mamba-7B-instruct --port $PORT
+#python last_ones/prompt_last_ones.py --ip_path datasets/500/first_or_last/data.jsonl --prompt_path prompts/last_ones/zero-shot_chat_last_v0 --cot "nocot" --save_path results/last_ones/falcon3-mamba-7B-instruct --port $PORT
 #python last_ones/prompt_last_ones.py --ip_path datasets/500/first_or_last/data.jsonl --prompt_path prompts/last_ones/zero-shot_chat_last_v0 --cot "cot" --save_path results/last_ones/falcon3-mamba-7B-instruct --port $PORT
 
-python inductionhead/prompt_inductionhead.py --ip_path datasets/500/flipflop_inductionhead/data.jsonl --prompt_path prompts/flipflop_inductionhead/inductionhead_zero-shot_chat_v0 --cot "nocot" --save_path results/flipflop_inductionhead/falcon3-mamba-7B-instruct --port $PORT --config "before"
+#python inductionhead/prompt_inductionhead.py --ip_path datasets/500/flipflop_inductionhead/data.jsonl --prompt_path prompts/flipflop_inductionhead/inductionhead_zero-shot_chat_v0 --cot "nocot" --save_path results/flipflop_inductionhead/falcon3-mamba-7B-instruct --port $PORT --config "before"
 #python inductionhead/prompt_inductionhead.py --ip_path datasets/500/flipflop_inductionhead/data.jsonl --prompt_path prompts/flipflop_inductionhead/inductionhead_zero-shot_chat_v0 --cot "cot" --save_path results/flipflop_inductionhead/falcon3-mamba-7B-instruct --port $PORT --config "before"
 
-python inductionhead/prompt_inductionhead.py --ip_path datasets/500/flipflop_inductionhead/data.jsonl --prompt_path prompts/flipflop_inductionhead/inductionhead_zero-shot_chat_v0 --cot "nocot" --save_path results/flipflop_inductionhead/falcon3-mamba-7B-instruct --port $PORT --config "after"
+#python inductionhead/prompt_inductionhead.py --ip_path datasets/500/flipflop_inductionhead/data.jsonl --prompt_path prompts/flipflop_inductionhead/inductionhead_zero-shot_chat_v0 --cot "nocot" --save_path results/flipflop_inductionhead/falcon3-mamba-7B-instruct --port $PORT --config "after"
 #python inductionhead/prompt_inductionhead.py --ip_path datasets/500/flipflop_inductionhead/data.jsonl --prompt_path prompts/flipflop_inductionhead/inductionhead_zero-shot_chat_v0 --cot "cot" --save_path results/flipflop_inductionhead/falcon3-mamba-7B-instruct --port $PORT --config "after"
 
 python flipflop/prompt_flipflop.py --ip_path datasets/500/flipflop/data.jsonl --prompt_path prompts/flipflop/flipflop_zero-shot_chat_v0 --cot "nocot" --save_path results/flipflop/falcon3-mamba-7B-instruct --port $PORT --config "fw-lc"
