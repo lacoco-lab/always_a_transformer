c3e7225697f77e09708aabcec73ad6bf9a5f3a56
Mayank Jobanputra <mayankjobanputra@gmail.com>
2025-03-06 15:57:24 +0100

    added tokenized inputs

--- utils/vllm_tokenize.py
+++ utils/vllm_tokenize.py
@@ -26,34 +26,42 @@ if __name__ == "__main__":
     registry = DirectoryPromptRegistry(Path(args.prompt_path), force_reindex=True)
     task_prompt = registry.get(name=f"task_{args.config}")
     sys_prompt = registry.get(name="sys")
-    
+
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     headers = {"Authorization": "Bearer sk_noreq", "Content-Type": "application/json"}
-    
+
     data = []
-    with jsonlines.open(input_file) as reader:    
+    with jsonlines.open(input_file) as reader:
         for example in tqdm(reader):
             prompt = task_prompt.text({"input": example["input"]})
-            prompt = [
-                {"role": "system", "content": sys_prompt.text()},
-                {"role": "user", "content": prompt}
-            ]
-            prompt_chat = tokenizer.apply_chat_template(prompt, tokenize=False)
-            prompt_tokens = tokenizer.tokenize(prompt_chat)
-             
+            if "chat" in args.prompt_path:
+                prompt = [
+                    {"role": "system", "content": sys_prompt.text()},
+                    {"role": "user", "content": prompt}
+                ]
+                prompt_chat = tokenizer.apply_chat_template(prompt, tokenize=False)
+                prompt_tokens = tokenizer.tokenize(prompt_chat)
+            else:
+                prompt_tokens = tokenizer.tokenize(prompt, add_special_tokens=True)
             # print(f"Prompt: {prompt}")
             # print(f"Prompt Tokens: {prompt_tokens}")
             # print(f"Length of Prompt Tokens: {len(prompt_tokens)} and Pre-length: {example["input_length"]}")
-            response = requests.post(f"http://0.0.0.0:{args.port}/tokenize",
-                                     headers=headers,
-                                     json={"messages": prompt,
-                                           "model": args.model,})
+            
+            if "chat" in args.prompt_path:
+                response = requests.post(f"http://0.0.0.0:{args.port}/tokenize",
+                                         headers=headers,
+                                         json={"messages": prompt,
+                                               "model": args.model,})
+            else:
+                response = requests.post(f"http://0.0.0.0:{args.port}/tokenize",
+                                         headers=headers,
+                                         json={"prompt": prompt,
+                                               "model": args.model,})
             ip_token_ids = response.json()["tokens"]
             ip_tokens = tokenizer.convert_ids_to_tokens(ip_token_ids)
             example["tokenized_input"] = ip_tokens
             assert len(ip_token_ids) == example["input_length"], f"Length mismatch: {len(ip_token_ids)} and {example['input_length']}"
             # print(f"Input Tokens: {ip_tokens}")
             data.append(example)
-    
     with jsonlines.open(input_file, "w") as writer:
         writer.write_all(data)
