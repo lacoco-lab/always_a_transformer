d9edcbfaca4fdeecf2c03076e6347e9e0e325852
Yana Veitsman <yana.veitsman@yandex.kz>
2025-03-10 18:40:34 +0100

    add llama bash

---
+++ circuit/llama8b.bash
@@ -0,0 +1,8 @@
+export HF_TOKEN=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
+
+source /scratch/yanav/anaconda3/bin/activate
+conda activate len-gen
+
+cd /nethome/yanav/repos/len-gen/circuit/
+
+python discover_ih.py
