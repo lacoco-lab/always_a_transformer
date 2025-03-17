05dcae1cb162236327f2c62677bbefa864c6439f
Yana Veitsman <yana.veitsman@yandex.kz>
2025-03-17 12:05:53 +0100

    finish ablation code

---
+++ circuit/ablate_heads.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export HF_TOKEN=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
+
+source /scratch/yanav/anaconda3/bin/activate
+conda activate len-gen
+
+cd /scratch/yanav/repos/len-gen/circuit/
+
+python ablate_heads.py -m llama -v non-instruct -t before -tp induction
+python ablate_heads.py -m llama -v non-instruct -t after -tp induction
+python ablate_heads.py -m llama -v instruct -t before -tp induction
+python ablate_heads.py -m llama -v instruct -t after -tp induction
