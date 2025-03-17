7d85c573c79e28abd1b803fa00822c733b4985a9
Yana Veitsman <yana.veitsman@yandex.kz>
2025-03-17 12:28:12 +0100

    check model

--- circuit/ablate_heads.py
+++ circuit/ablate_heads.py
@@ -31,6 +31,7 @@ template_str = "{{ system }} {{ user_input }}:"
 system_path = 'templates/system.jinja'
 template = Template(template_str)
 heads_to_ablate = load_heads(model_name)
+print(model.cfg)
 
 answers = []
 for example in data:
