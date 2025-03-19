bfec501f451ad70cf2b1dbe310313c4338386197
Yana Veitsman <yana.veitsman@yandex.kz>
2025-03-19 10:14:14 +0100

    separate bash files and etc

---
+++ circuit/bash/ablate_heads.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export HF_TOKEN=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
+
+source /scratch/yanav/anaconda3/bin/activate
+conda activate len-gen
+
+cd /scratch/yanav/repos/len-gen/circuit/
+
+python ablate_heads.py -m llama -v instruct -t before -tp induction
+python ablate_heads.py -m llama -v instruct -t after -tp induction
