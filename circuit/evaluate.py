85dff5eb8186eac4d30401efcbd2ace71865dd73
Yana Veitsman <yana.veitsman@yandex.kz>
2025-03-31 19:12:41 +0200

    add more heads

--- circuit/evaluate.py
+++ circuit/evaluate.py
@@ -12,10 +12,8 @@ def extract_answer(ans):
     match = re.search(r"<ans>(.*?)</ans>", ans['full_answer'])
     if match:
         extracted = match.group(1)
-        ans['answer'] = extracted
-    else:
-        ans['answer'] = None
-    return ans
+        return extracted
+    return None
 
 
 def count_distribution(answers):
@@ -42,7 +40,8 @@ def clean_results(results):
     :return: arr 
     """
     for line in results:
-        line['answer'] = line['full_answer'][0]   
+        line['answer'] = extract_answer(line)
+        print(line)
     return results
 
 
@@ -61,7 +60,7 @@ def get_accuracy(results):
 
 #llama_after_anti = clean_results(get_data("results/llama_non-instruct_after_random-all_100.jsonl"))
 #llama_before_anti = clean_results(get_data("results/llama_non-instruct_before_anti-induction.jsonl"))
-llama_after = clean_results(get_data("results/gemma_instruct_after_random-all_20.jsonl"))
+llama_after = clean_results(get_data("results/gemma_instruct_after_induction_20.jsonl"))
 #llama_before = clean_results(get_data("results/llama_non-instruct_before_induction.jsonl"))
 
 #llama_instruct_after_anti = [extract_answer(ans) for ans in clean_results(get_data("results/llama_instruct_after_anti-induction.jsonl"))]
