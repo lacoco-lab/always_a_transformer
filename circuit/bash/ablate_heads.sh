ac13b96096aa8b1b4b98a7eb8e7781e62711ebd6
Yana Veitsman <yana.veitsman@yandex.kz>
2025-03-31 17:53:45 +0200

    ablate heads

--- circuit/bash/ablate_heads.sh
+++ circuit/bash/ablate_heads.sh
@@ -6,5 +6,5 @@ conda activate len-gen
 
 cd /scratch/yanav/repos/len-gen/circuit/
 
-python ablate_heads.py -m gemma -v instruct -t after -tp random-all -l 20
-python ablate_heads.py -m gemma -v instruct -t after -tp random-all -l 50
+python ablate_heads.py -m gemma -v instruct -t after -tp induction -l 20
+python ablate_heads.py -m gemma -v instruct -t after -tp induction -l 50
