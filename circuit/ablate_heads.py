25ff20ef496e0e7a3fb16b1951ad62c355934f3f
Yana Veitsman <yana.veitsman@yandex.kz>
2025-04-03 10:16:43 +0200

    gemma maps

--- circuit/ablate_heads.py
+++ circuit/ablate_heads.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from jinja2 import Template
 from transformer_lens import HookedTransformer
+from transformer_lens.utils import get_act_name
 from argparse import ArgumentParser
 import jsonlines
 from utils import combine_params, get_data, load_heads, render_prompt, get_gold_ans
@@ -42,33 +43,6 @@ def check_logits_for_next_token(model: HookedTransformer, tokens, topk: int = 5)
         print(f"  Token: {repr(token_str)} | Logit: {float(val):.4f}")
     print()
 
-def plot_attention_map(attention_data, tokens, output_dir, layer, head):
-    """Plot and save attention map for the specified layer and head."""
-    plt.figure(figsize=(8, 8))
-    plt.imshow(attention_data, cmap='viridis', aspect='auto')
-    plt.colorbar()
-    token_labels = [model.to_string([t]) for t in tokens]
-    plt.xticks(ticks=np.arange(len(token_labels)), labels=token_labels, rotation=90)
-    plt.yticks(ticks=np.arange(len(token_labels)), labels=token_labels)
-    plt.title(f"Attention from token 'w' to next token\nLayer {layer}, Head {head}")
-    
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    
-    attention_plot_path = os.path.join(output_dir, f"attention_layer{layer}_head{head}.png")
-    plt.tight_layout()
-    plt.savefig(attention_plot_path)
-    plt.close()
-
-def capture_attention_hook(layer: int, head: int):
-    """Hook to capture attention data for a specific head."""
-    def hook(value, hook):
-        # Capture attention data from hook_values (shape: [batch_size, num_heads, seq_len, seq_len])
-        attention_data = value # Attention for all heads
-        return attention_data
-    return hook
-
-
 parser = ArgumentParser()
 parser.add_argument("-m", "--model", dest="model", help="choose model")
 parser.add_argument("-v", "--version", dest="version", help="instruct or non-instruct model version")
@@ -86,6 +60,7 @@ set_global_seed(args.seed)
 
 model = HookedTransformer.from_pretrained(model_name)
 model.cfg.use_cache = False
+tokenizer = model.tokenizer
 
 data = get_data(data_path)[:100]
 inp_length = len(data[0]['input'])
@@ -104,6 +79,7 @@ print(f"Random seed: {args.seed}, top_k: {args.top_k}")
 
 answers = []
 output_dir = 'attention_plots'
+attention_scores = {}
 
 for example in data:
     if args.version == 'non-instruct':
@@ -114,24 +90,17 @@ for example in data:
     system_prompt, task_prompt = render_prompt(system_path, task_path, example['input'])
     prompt = template.render(system=system_prompt, user_input=task_prompt)
     tokens = model.to_tokens(prompt)
-    token_strs = model.to_string(tokens[0])
+    w_token_id = tokenizer.encode('w', add_special_tokens=False)[0]
     
     try:
-        w_index = token_strs.index('w')
-        next_token_index = w_index + 1
+        w_indices = [i for i, x in enumerate(tokens[0]) if x == w_token_id]
     except ValueError:
-        print(f"Token 'w' not found in the input: {token_strs}")
+        print(f"Token 'w' not found in the input: {tokens[0]}")
         continue
-    
-    print(f"Found 'w' at index {w_index}, next token at index {next_token_index}")
 
     hooks = [
         (f'blocks.{layer}.attn.hook_z', ablate_head_hook(layer, head))
         for layer, head in heads_to_ablate
-    ]
-    capture_hooks = [
-        (f'blocks.{layer}.attn.hook_pattern', capture_attention_hook(layer, head))
-        for layer in range(model.cfg.n_layers) for head in range(model.cfg.n_heads)
     ]
     
     original_loss = model(tokens, return_type="loss")
@@ -141,51 +110,56 @@ for example in data:
         fwd_hooks=hooks
     )
 
-    print(f"Original Loss: {original_loss.item():.3f}")
-    print(f"Ablated Loss: {ablated_loss.item():.3f}")
+    #print(f"Original Loss: {original_loss.item():.3f}")
+    #print(f"Ablated Loss: {ablated_loss.item():.3f}")
 
-    attention_scores = {}
-    with model.hooks(fwd_hooks=hooks+capture_hooks):
-        generated_tokens = model.generate(
-                tokens,
-                max_new_tokens=max_new,
-                stop_at_eos=True,
-                do_sample=False,
-                top_k=args.top_k,
-                temperature=0, 
-            )
+    _, cache = model.run_with_cache(
+            tokens,
+            names_filter=lambda name: "attn" in name,
+            return_type=None
+        )
 
-        for layer in range(model.cfg.n_layers):
-            for head in range(model.cfg.n_heads):
-                # Access the captured attention data from the hook
-                attention_data = model.hooks[f'blocks.{layer}.attn.hook_pattern'][0, :, head, :].detach().cpu().numpy()
-                attention_score = attention_data[w_index, next_token_index]  # Focus on attention from 'w' to the next token
-                attention_scores[(layer, head)] = attention_score
-  
-        sorted_attention_heads = sorted(attention_scores.items(), key=lambda x: x[1], reverse=True)[:10]
-        for (layer, head), score in sorted_attention_heads:
-            print(f"Saving attention map for Layer {layer}, Head {head} with score {score:.4f}")
-            attention_data = model.attn_data[layer, head, w_index, next_token_index]
-            plot_attention_map(attention_data, tokens, output_dir, layer, head)
+    for layer in range(model.cfg.n_layers):
+        for head in range(model.cfg.n_heads):
+            attn_patterns = cache[get_act_name("attn", layer)]
+            head_patterns = attn_patterns[:, head, :, :]
+            if (layer, head) in attention_scores.keys():
+                attention_scores[(layer, head)] += head_patterns[0, w_indices[1], w_indices[0]].item()
+            else:
+                attention_scores[(layer, head)] = head_patterns[0, w_indices[1], w_indices[0]].item()
 
-    new_tokens = generated_tokens[0, tokens.shape[-1]:]
-    generated_text = model.to_string(new_tokens)
+    
+    # with model.hooks(fwd_hooks=hooks):
+    #     generated_tokens = model.generate(
+    #             tokens,
+    #             max_new_tokens=max_new,
+    #             stop_at_eos=True,
+    #             do_sample=False,
+    #             top_k=args.top_k,
+    #             temperature=0, 
+    #         )
 
-    answer = {
-        'input': example['input'],
-        'gold_ans_char': get_gold_ans(example['input'], args.task),
-        'full_answer': generated_text
-    }
-    answers.append(answer)
+    # new_tokens = generated_tokens[0, tokens.shape[-1]:]
+    # generated_text = model.to_string(new_tokens)
 
-output_path = (
-    'results/'
-    + args.model + '_'
-    + args.version + '_'
-    + args.task + '_'
-    + args.type + "_"
-    + str(inp_length)
-    + '.jsonl'
-)
-with jsonlines.open(output_path, mode='w') as writer:
-    writer.write_all(answers)
+    # answer = {
+    #     'input': example['input'],
+    #     'gold_ans_char': get_gold_ans(example['input'], args.task),
+    #     'full_answer': generated_text
+    # }
+    # answers.append(answer)
+sorted_attention_heads = sorted(attention_scores.items(), key=lambda x: x[1], reverse=True)[:10]
+for (layer, head), score in sorted_attention_heads:
+    print(f"Attention for Layer {layer}, Head {head} with score {score:.4f}")
+
+# output_path = (
+#     'results/'
+#     + args.model + '_'
+#     + args.version + '_'
+#     + args.task + '_'
+#     + args.type + "_"
+#     + str(inp_length)
+#     + '.jsonl'
+# )
+# with jsonlines.open(output_path, mode='w') as writer:
+#     writer.write_all(answers)
